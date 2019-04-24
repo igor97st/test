bc3f133a2b116e083ad9b299da0f2248bb644e37
igor97st <x00128490@gmail.com>
2019-04-24 20:15:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var request = require("request")
 var port = process.env.PORT || 3000;
 app.get('/a', (req,res) => res.send("hello!"));
 
-app.get('/u', (req, res) => {
+app.get('/api/upc', (req, res) => {
   const url = "https://api.themoviedb.org/3/movie/upcoming?api_key=60a27eb65f7bfc6491658e507c3c57ec&language=en-US&page=1"
   request({
     url: url,
