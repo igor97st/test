324c746f02fa5d112ecf8060605c73f4dd1c040b
igor97st <x00128490@gmail.com>
2019-04-24 19:54:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+var request = require("request")
+
 var port = process.env.PORT || 3000;
 app.get('/a', (req,res) => res.send("hello!"));
+
+app.get('/u', (req, res) => {
+  const url = "https://api.themoviedb.org/3/movie/upcoming?api_key=60a27eb65f7bfc6491658e507c3c57ec&language=en-US&page=1"
+  request({
+    url: url,
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      res.json(body.results);
+    }
+  })
+});
+
 app.listen(port, () => console.log("running"));
