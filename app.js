e7806b618cc24314d98d8c636b52627f456514c1
igor97st <x00128490@gmail.com>
2019-04-27 16:38:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,18 @@ firebase.initializeApp(config);
 this.database = firebase.database().ref().child('Liked');
 
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 app.get('/api/recommend/:userId', (req, res) => {
   var owner = req.params.userId;
